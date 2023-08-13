7a5c83b3e924c5b3e9a9bd446f15b779ca6772e9
370025263 <370025263@qq.com>
2023-08-13 16:58:11 +0800

    [release]多平台兼容性

--- release/windows_ver/start_engine.bat
+++ release/windows_ver/start_engine.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 :: 定义Python解释器和脚本的路径
 set PYTHON_EXE_PATH="python_lib/python-3.9.6-embed-amd64/python.exe"
-set PYTHON_SCRIPT_PATH="code/npc_engine/src/main.py"
+set PYTHON_SCRIPT_PATH="code/npc_engine/src/engine.py"
 
 :: 检查Python解释器是否存在
 if not exist %PYTHON_EXE_PATH% (
